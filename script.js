5c6a5840ea807e0e284efba74b0cece6982ad05e
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-27 16:58:25 +0530

    Create a function to call API endpoint

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('working...');
+const API_KEY = '32be7ec8fb314575c5442974e415a105';
+
+async function weatherOf(location) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}`
+  );
+
+  const locationData = await response.json();
+
+  return locationData;
+}

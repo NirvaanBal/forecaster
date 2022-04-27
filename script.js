9409c54ff3bf70346021fc698f5f09e789c83f9e
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-27 17:04:15 +0530

    Update API endpoint to get data in celcius

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const API_KEY = '32be7ec8fb314575c5442974e415a105';
 
 async function weatherOf(location) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}&units=metric`
   );
 
-  const locationData = await response.json();
+  const data = await response.json();
 
-  return locationData;
+  return data;
 }
